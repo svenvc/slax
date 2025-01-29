a7ced9407e36b2d907fb2dfafdfed95a17d1ca67
Sven Van Caekenberghe <sven@beta9.be>
2025-01-29 15:47:02 +0100

    Add href to links

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -56,7 +56,7 @@ defmodule SlaxWeb.ChatRoomLive do
         "flex items-center h-8 text-sm pl-8 pr-3",
         (@active && "bg-slate-300") || "hover:bg-slate-300"
       ]}
-      href="#"
+      href={~p"/rooms/#{@room}"}
     >
       <.icon name="hero-hashtag" class="h-4 w-4" />
       <span class={["ml-2 leading-none", @active && "font-bold"]}>
@@ -66,9 +66,18 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
-  def mount(_params, _session, socket) do
+  def mount(params, _session, socket) do
     rooms = Repo.all(Room)
-    room = List.first(rooms)
+
+    room =
+      case Map.fetch(params, "id") do
+        {:ok, id} ->
+          %Room{} = Enum.find(rooms, fn x -> to_string(x.id) == id end)
+
+        :error ->
+          List.first(rooms)
+      end
+
     {:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
   end
 
