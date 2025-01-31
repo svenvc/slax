775f200a699c1a82385516a787f3fce23ca57c10
Sven Van Caekenberghe <sven@beta9.be>
2025-01-31 14:01:13 +0100

    Use Ecto.Query in Chat context

--- lib/slax/chat.ex
+++ lib/slax/chat.ex
@@ -2,7 +2,9 @@ defmodule Slax.Chat do
   alias Slax.Chat.Room
   alias Slax.Repo
 
+  import Ecto.Query
+
   def list_rooms do
-    Repo.all(Room)
+    Repo.all(from Room, order_by: [asc: :name])
   end
 end
