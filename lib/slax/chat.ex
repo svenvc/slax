14b72147f43eca4d884a08ed52d0b85ca282eee5
Sven Van Caekenberghe <sven@beta9.be>
2025-02-04 10:11:26 +0100

    Show room joined status on index page

--- lib/slax/chat.ex
+++ lib/slax/chat.ex
@@ -18,6 +18,16 @@ defmodule Slax.Chat do
     |> Enum.sort_by(& &1.name)
   end
 
+  def list_rooms_with_joined(%User{} = user) do
+    query =
+      from r in Room,
+        left_join: m in RoomMembership,
+        on: r.id == m.room_id and m.user_id == ^user.id,
+        select: {r, not is_nil(m.id)},
+        order_by: [asc: :name]
+
+    Repo.all(query)
+  end
   def create_room(attrs) do
     %Room{}
     |> Room.changeset(attrs)
--- lib/slax_web/live/chat_room_live/index.ex
+++ lib/slax_web/live/chat_room_live/index.ex
@@ -12,7 +12,7 @@ defmodule SlaxWeb.ChatRoomLive.Index do
       <div class="bg-slate-50 border rounded">
         <div id="rooms" class="divide-y" phx-update="stream">
           <.link
-            :for={{id, room} <- @streams.rooms}
+            :for={{id, {room, joined?}} <- @streams.rooms}
             class="cursor-pointer p-4 flex justify-between items-center group first:rounded-t last:rounded-b"
             id={id}
             navigate={~p"/rooms/#{room}"}
@@ -25,6 +25,12 @@ defmodule SlaxWeb.ChatRoomLive.Index do
                 </span>
               </div>
               <div class="text-gray-500 text-sm">
+                <%= if joined? do %>
+                  <span class="text-green-600 font-bold">✓ Joined</span>
+                <% end %>
+                <%= if joined? && room.topic do %>
+                  <span class="mx-1">·</span>
+                <% end %>
                 <%= if room.topic do %>
                   {room.topic}
                 <% end %>
@@ -38,8 +44,12 @@ defmodule SlaxWeb.ChatRoomLive.Index do
   end
 
   def mount(_params, _session, socket) do
-    rooms = Chat.list_rooms()
-    socket = socket |> assign(page_title: "All rooms") |> stream(:rooms, rooms)
+    rooms = Chat.list_rooms_with_joined(socket.assigns.current_user)
+    socket =
+      socket
+      |> assign(page_title: "All rooms")
+      |> stream_configure(:rooms, dom_id: fn {room, _} -> "rooms-#{room.id}" end)
+      |> stream(:rooms, rooms)
     {:ok, socket}
   end
 end
