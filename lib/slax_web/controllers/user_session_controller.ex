5eebc81a3024a477461adf583212e014a58a19ee
Sven Van Caekenberghe <sven@beta9.be>
2025-02-05 15:25:31 +0100

    Allow login with either username or email

--- lib/slax_web/controllers/user_session_controller.ex
+++ lib/slax_web/controllers/user_session_controller.ex
@@ -5,6 +5,8 @@ defmodule SlaxWeb.UserSessionController do
   alias SlaxWeb.UserAuth
 
   def create(conn, %{"_action" => "registered"} = params) do
+    params = put_in(params, ["user", "email_or_username"], params["user"]["email"])
+
     create(conn, params, "Account created successfully!")
   end
 
@@ -19,17 +21,17 @@ defmodule SlaxWeb.UserSessionController do
   end
 
   defp create(conn, %{"user" => user_params}, info) do
-    %{"email" => email, "password" => password} = user_params
+    %{"email_or_username" => email_or_username, "password" => password} = user_params
 
-    if user = Accounts.get_user_by_email_and_password(email, password) do
+    if user = Accounts.get_authenticated_user(email_or_username, password) do
       conn
       |> put_flash(:info, info)
       |> UserAuth.log_in_user(user, user_params)
     else
-      # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
+      # In order to prevent user enumeration attacks, don't disclose whether the email or username is registered.
       conn
-      |> put_flash(:error, "Invalid email or password")
-      |> put_flash(:email, String.slice(email, 0, 160))
+      |> put_flash(:error, "Invalid login")
+      |> put_flash(:email_or_username, String.slice(email_or_username, 0, 160))
       |> redirect(to: ~p"/users/log_in")
     end
   end
